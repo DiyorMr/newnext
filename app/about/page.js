1eb0e057fd4d1ae9f3751cfa3acd0f80e3943e9e
DiyorMr <dmuxiddinov594@gmail.com>
2026-07-16 19:38:41 +0500

    pages done

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main>
+      <h1>Bu About sahifa</h1>
+    </main>
+  );
+}
---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main>
+      <h1>Bu Contact sahifa</h1>
+    </main>
+  );
+}
